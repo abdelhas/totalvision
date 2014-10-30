4179d3658cb7e28455cf7ea744971d6a96057c25
EC2 Default User <ec2-user@ip-10-252-153-83.us-west-2.compute.internal>
2014-10-30 06:08:14 +0000

    max zoom fix

--- public/js/controllers/swipe.js
+++ public/js/controllers/swipe.js
@@ -1,8 +1,8 @@
 var map = L.map("map");
 //Default layer
-L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
 
-var overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png').addTo(map);
+var overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
 var range = document.getElementById('swiper');
 var slider = document.getElementById('slider');
 
@@ -20,8 +20,8 @@ function changeLayer() {
 	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2041/{z}/{x}/{y}.png');
 	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2070/{z}/{x}/{y}.png');
 	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2099/{z}/{x}/{y}.png');
-	L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png').addTo(map);
-	overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/' +slider.value + '/{z}/{x}/{y}.png').addTo(map);    
+	L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
+	overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/' +slider.value + '/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);    
 	$(".leftMapYear").text(slider.value);
 	clip();
 };

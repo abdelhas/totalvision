2b5b3a6a5a6b3b8066bad7266b5ea7f9c6df041a
EC2 Default User <ec2-user@ip-10-252-153-83.us-west-2.compute.internal>
2014-11-02 19:41:02 +0000

    Updated domain name

--- public/js/controllers/swipe.js
+++ public/js/controllers/swipe.js
@@ -1,8 +1,8 @@
 var map = L.map("map");
 //Default layer
-L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
+L.tileLayer('/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
 
-var overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
+var overlay = L.tileLayer('/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
 var range = document.getElementById('swiper');
 var slider = document.getElementById('slider');
 
@@ -17,11 +17,11 @@ function clip() {
 function changeLayer() {
 	var yearSelected = $("#slider option:selected").text(); 
 	console.log('here: ' + slider.value);
-	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2041/{z}/{x}/{y}.png');
-	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2070/{z}/{x}/{y}.png');
-	map.removeLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2099/{z}/{x}/{y}.png');
-	L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
-	overlay = L.tileLayer('http://ec2-54-245-62-84.us-west-2.compute.amazonaws.com/mapdata/' +slider.value + '/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);    
+	map.removeLayer('/mapdata/2041/{z}/{x}/{y}.png');
+	map.removeLayer('/mapdata/2070/{z}/{x}/{y}.png');
+	map.removeLayer('/mapdata/2099/{z}/{x}/{y}.png');
+	L.tileLayer('/mapdata/2012/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);
+	overlay = L.tileLayer('/mapdata/' +slider.value + '/{z}/{x}/{y}.png', { maxZoom: 5 }).addTo(map);    
 	$(".leftMapYear").text(slider.value);
 	clip();
 };
